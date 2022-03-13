0f18b97d1ac067e630b9fcbcbd5e45a91b5ecbb1
chamomile <55081468+cat-boop@users.noreply.github.com>
2022-03-13 22:52:17 +0300

    delete useless constructor

--- lab5/lab-client/src/main/java/com/lab/client/Exceptions/RouteValidateException.java
+++ lab5/lab-client/src/main/java/com/lab/client/Exceptions/RouteValidateException.java
@@ -1,10 +1,6 @@
 package com.lab.client.Exceptions;
 
 public class RouteValidateException extends RuntimeException {
-    public RouteValidateException() {
-        this("Ошибка при валидации пути");
-    }
-
     public RouteValidateException(String message) {
         super(message);
     }
