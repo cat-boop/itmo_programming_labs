2048532b095653e1b1e3bab4728ad694961362f8
chamomile <55081468+cat-boop@users.noreply.github.com>
2022-08-06 17:40:36 +0300

    remove lab5 and lab6

--- lab6/lab-common/src/main/java/com/lab/common/Exceptions/DeserializeException.java
+++
@@ -1,7 +0,0 @@
-package com.lab.common.Exceptions;
-
-public class DeserializeException extends RuntimeException {
-    public DeserializeException(String message) {
-        super(message);
-    }
-}
--- lab6/lab-common/src/main/java/com/lab/common/Exceptions/SerializeException.java
+++
@@ -1,7 +0,0 @@
-package com.lab.common.Exceptions;
-
-public class SerializeException extends RuntimeException {
-    public SerializeException(String message) {
-        super(message);
-    }
-}
